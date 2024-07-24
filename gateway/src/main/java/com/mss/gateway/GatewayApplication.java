f71c6e4b83a65981513f37b77021af29291300bf
michael.johnson <michael.johnson@turntabl.io>
2024-07-24 22:43:00 +0000

    --worked on the minor update

--- gateway/src/main/java/com/mss/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/mss/gateway/GatewayApplication.java
@@ -10,19 +10,6 @@ import org.springframework.context.annotation.Bean;
 @EnableEurekaServer
 public class GatewayApplication {
 
-	/*@Bean
-	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route("service1", r -> r.path("/service1/**")
-						.filters(f -> f.stripPrefix(1))
-						.uri("lb://SERVICE-NAME1"))
-				.route("service2", r -> r.path("/service2/**")
-						.filters(f -> f.stripPrefix(1))
-						.uri("lb://SERVICE-NAME2"))
-				.build();
-	}*/
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
